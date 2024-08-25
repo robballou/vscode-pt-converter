4f4c16f9664ec873446e4b4a5b77bf0e6827dbaa
Rob Ballou <rob.ballou@bombbomb.com>
2024-08-25 11:20:52 -0600

    Add support for defaultProps and replacing functional components

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,114 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { createTypeForComponent } from "proptype-converter";
+import {
+	ComponentPropTypes,
+	createPropsForComponent,
+	createTypeForComponent,
+} from "proptype-converter";
 import { processDocument } from "./processDocument";
 
 class PTConverterActionProvider implements vscode.CodeActionProvider {
+	/**
+	 * Return the code action to convert just the proptypes...
+	 */
+	getConvertPropTypesAction(
+		key: string,
+		value: ComponentPropTypes,
+		document: vscode.TextDocument,
+	) {
+		const convertAction = new vscode.CodeAction(
+			`Convert PropTypes: ${key}`,
+			vscode.CodeActionKind.RefactorRewrite,
+		);
+		convertAction.edit = new vscode.WorkspaceEdit();
+		const types = createTypeForComponent(key, value);
+		this.addTypeEdits(convertAction.edit, types, value, document);
+
+		// rename the file to TS
+		const newName = this.getNewFileName(document.uri);
+		if (newName) {
+			convertAction.edit.renameFile(document.uri, newName);
+		}
+		return convertAction;
+	}
+
+	getConvertPropTypesWithDefaultPropsAction(
+		key: string,
+		value: ComponentPropTypes,
+		document: vscode.TextDocument,
+	) {
+		if (!value.defaultProps) {
+			return null;
+		}
+
+		const props = createPropsForComponent(value);
+		if (!props) {
+			return null;
+		}
+
+		const convertAction = new vscode.CodeAction(
+			`Convert PropTypes and defaultProps: ${key}`,
+			vscode.CodeActionKind.RefactorRewrite,
+		);
+		convertAction.edit = new vscode.WorkspaceEdit();
+		const types = createTypeForComponent(key, value);
+
+		// delete the existing .defaultProps
+		if (value.defaultPropsRange) {
+			convertAction.edit.delete(
+				document.uri,
+				textRangeToRange(document, value.defaultPropsRange),
+			);
+		}
+
+		// replace the functional components first argument with the new props (and defaults)
+		if (value.parameterRange) {
+			convertAction.edit.replace(
+				document.uri,
+				textRangeToRange(document, value.parameterRange),
+				`${props}: ${key}Props`,
+			);
+		}
+
+		this.addTypeEdits(convertAction.edit, types, value, document);
+
+		const newName = this.getNewFileName(document.uri);
+		if (newName) {
+			convertAction.edit.renameFile(document.uri, newName);
+		}
+		return convertAction;
+	}
+
+	addTypeEdits(
+		edit: vscode.WorkspaceEdit,
+		types: string,
+		value: ComponentPropTypes,
+		document: vscode.TextDocument,
+	) {
+		// if we identified the component, we can put the type above it
+		if (value.componentRange) {
+			edit.delete(document.uri, textRangeToRange(document, value.range));
+			edit.insert(
+				document.uri,
+				document.positionAt(value.componentRange[0]),
+				`${types}\n\n`,
+			);
+		} else {
+			edit.replace(
+				document.uri,
+				textRangeToRange(document, value.range),
+				types,
+			);
+		}
+	}
+
+	getNewFileName(currentUri: vscode.TextDocument["uri"]): vscode.Uri | null {
+		const fileName = currentUri.fsPath.replace(/\.j(sx?)$/, ".t$1");
+		const newName = vscode.Uri.file(fileName);
+		return newName.fsPath !== currentUri.fsPath ? newName : null;
+	}
+
 	provideCodeActions(
 		document: vscode.TextDocument,
 		range: vscode.Range | vscode.Selection,
@@ -19,38 +123,18 @@ class PTConverterActionProvider implements vscode.CodeActionProvider {
 		if (result && result.size > 0) {
 			const actions: vscode.ProviderResult<vscode.CodeAction[]> = [];
 			result.forEach((value, key) => {
-				const convertAction = new vscode.CodeAction(
-					`Convert PropTypes: ${key}`,
-					vscode.CodeActionKind.RefactorRewrite,
+				const convertAction = this.getConvertPropTypesAction(
+					key,
+					value,
+					document,
 				);
-				convertAction.edit = new vscode.WorkspaceEdit();
-				const types = createTypeForComponent(key, value);
-
-				// if we identified the component, we can put the type above it
-				if (value.componentRange) {
-					convertAction.edit.delete(
-						document.uri,
-						textRangeToRange(document, value.range),
-					);
-					convertAction.edit.insert(
-						document.uri,
-						document.positionAt(value.componentRange[0]),
-						`${types}\n\n`,
-					);
-				} else {
-					convertAction.edit.replace(
-						document.uri,
-						textRangeToRange(document, value.range),
-						types,
-					);
-				}
-
-				const fileName = document.uri.fsPath.replace(/\.js$/, ".ts");
-				const newName = vscode.Uri.file(fileName);
-				if (newName.fsPath !== document.uri.fsPath) {
-					convertAction.edit.renameFile(document.uri, newName);
-				}
 				actions.push(convertAction);
+
+				const convertWithDefaultPropsAction =
+					this.getConvertPropTypesWithDefaultPropsAction(key, value, document);
+				if (convertWithDefaultPropsAction) {
+					actions.push(convertWithDefaultPropsAction);
+				}
 			});
 			return actions;
 		}
